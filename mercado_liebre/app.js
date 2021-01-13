9b9e39cbcec4071982667d11f6f7b4dfaf1e90a9
emapellizza <emapellizza@gmail.com>
2021-01-13 12:34:09 -0300

    proyecto mercado liebre

---
+++ mercado_liebre/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.listen(process.env.PORT ||3000,()=>{ console.log('server ML run')});
+
+app.use(express.static(path.join(__dirname,'./public')));
+
+app.get('/',(req,res)=>{
+
+  res.sendFile(path.join(__dirname,'./views/home.html'))
+});
+
+app.get('/login',(req,res)=> {
+
+  res.sendFile(path.join(__dirname,'./views/login.html'))
+});
+
+app.get('/register',(req,res)=> {
+
+  res.sendFile(path.join(__dirname,'./views/register.html'))
+});
